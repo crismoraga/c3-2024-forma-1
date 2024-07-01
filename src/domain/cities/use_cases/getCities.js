038d6ebfccccbb5b2e691b14f495b081416f3a3e
crismoraga <cristobal.moragag@usm.cl>
2024-07-01 19:49:11 -0400

    Resolución Control 3 TEL335 Cristóbal Moraga

--- src/domain/cities/use_cases/getCities.js
+++ src/domain/cities/use_cases/getCities.js
@@ -1,16 +1,48 @@
+import { status } from 'koa/lib/response'
 import citiesRepository from '../repository/worldCitiesRespository'
 
 exports.getAllCitiesUseCase = (ctx) => {
-    ctx.body = citiesRepository.getAllCitiesRepository()
-    return ctx
+    ctx.body = citiesRepository.getAllCitiesRepository();
+    if (ctx.params.country.length < 3 || ctx.params.city.length < 3) {
+        ctx.status = 400;
+        ctx.body = { "message": "El país/ciudad ingresado debe tener al menos 3 caracteres" };
+    } else {
+        ctx.status = 200;
+    }
+    return ctx;
 }
 
 exports.getCitiesByCountryUseCase = (ctx) => {
-    ctx.body = citiesRepository.searchCitiesByCountryName(ctx.params.country)
-    return ctx
+    ctx.body = citiesRepository.searchCitiesByCountryName(ctx.params.country);
+    var regex = /^[A-Za-z]+$/;
+
+    if (!regex.test(ctx.params.country)){
+        if(Number.isInteger(parseInt(ctx.params.country))){
+        ctx.status = 400;
+        ctx.body = { "message": "Solo se aceptan caracteres no numéricos" };
+        return ctx;
+        }
+    } else if (ctx.params.country.length < 3) {
+        ctx.status = 400;
+        ctx.body = { "message": "El país/ciudad ingresado debe tener al menos 3 caracteres" };
+        return ctx;
+    } else if (ctx.body.length === 0) {
+        ctx.status = 200;
+        ctx.body = { "message": "No se encontraron ciudades para el país ingresado" };
+        return ctx;
+    } else {
+        ctx.status = 200;
+    }
+    return ctx;
 }
 
 exports.getCitiesByCityNameAndCountryUseCase = (ctx) => {
-    ctx.body = citiesRepository.searchCityByCityNameAndCountry(ctx.params.city, ctx.params.country)
-    return ctx
+    ctx.body = citiesRepository.searchCityByCityNameAndCountry(ctx.params.city, ctx.params.country);
+    if (ctx.params.country.length < 3 || ctx.params.city.length < 3) {
+        ctx.status = 400;
+        ctx.body = { "message": "El país/ciudad ingresado debe tener al menos 3 caracteres" };
+    } else {
+        ctx.status = 200;
+    }
+    return ctx;
 }
--- tests/routes/health/GETHealth.integration.test.js
+++ tests/routes/health/GETHealth.integration.test.js
@@ -16,3 +16,33 @@ describe('GET /health', () => {
         expect(response.body).toEqual({ message: 'ok' })
     })
 })
+
+describe('consulta GET /api/cities/by_country/:country', () => {
+    test('Debería devolver un arreglo de ciudades al ingresar un parámetro válido', async () => {
+        const response = await request(app.callback()).get('/api/cities/by_country/Spain')
+        expect(response.status).toBe(200)
+        expect(Array.isArray(response.body)).toBe(true)
+    })
+    
+    test('Debería devolver un arreglo vacío de ciudades al ingresar un parámetro válido pero no existente', async () => {
+        const response = await request(app.callback()).get('/api/cities/by_country/NonExistentCountry')
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({ message: 'No se encontraron ciudades para el país ingresado' })
+    })
+    
+    test('Debería devolver el mensaje "Solo se aceptan caracteres no numéricos" al ingresar un número', async () => {
+        const response = await request(app.callback()).get('/api/cities/by_country/12345')
+        expect(response.body).toEqual({ message: 'Solo se aceptan caracteres no numéricos' })
+        expect(response.status).toBe(400)
+    })
+    test('Debería devolver el mensaje "Solo se aceptan caracteres no numéricos" al ingresar un número con letras', async () => {
+        const response = await request(app.callback()).get('/api/cities/by_country/1a2b3c4d5e')
+        expect(response.body).toEqual({ message: 'Solo se aceptan caracteres no numéricos' })
+        expect(response.status).toBe(400)
+    })
+    test('Debería devolver el mensaje "El país/ciudad ingresado debe tener al menos 3 caracteres" al ingresar un país o ciudad con menos de 3 carácteres', async () => {
+        const response = await request(app.callback()).get('/api/cities/by_country/AB')
+        expect(response.status).toBe(400)
+        expect(response.body).toEqual({ message: 'El país/ciudad ingresado debe tener al menos 3 caracteres' })
+    })
+})
